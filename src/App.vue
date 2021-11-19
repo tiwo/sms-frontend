441440b12f4ee05d15fbd555fac129d4fe9028d4
tiwo <tiwocode@gmail.com>
2021-11-20 00:55:35 +0100

    add HelpfulHackingDisplay component

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,12 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import HelloWorld from './components/HelloWorld.vue'
+import HelpfulHackingDisplay from './components/HelpfulHackingDisplay.vue';
 </script>
 
 <template>
+  <HelpfulHackingDisplay />
+  <hr />
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + Vite" />
 </template>
---
+++ src/components/HelpfulHackingDisplay.vue
@@ -0,0 +1,12 @@
+<template>
+<div class="HelpfulHackingDisplay">
+    <h3>Hacking Display</h3>
+</div>
+</template>
+
+<style scoped>
+.HelpfulHackingDisplay {
+    background: #eff;
+    border: 1px dashed greenyellow;
+}
+</style>
