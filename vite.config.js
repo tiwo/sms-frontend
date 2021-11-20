83353d95cbbdb2aa374348b6073cf31ca2119822
tiwo <tiwocode@gmail.com>
2021-11-20 10:23:03 +0100

    build for deployment under ...github.io/sms-frontend

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 // https://vitejs.dev/config/
 // https://quasar.dev/start/vite-plugin#using-quasar
 export default defineConfig({
+  base: '/sms-frontend/',
   plugins: [
     vue({
       template: { transformAssetUrls },
