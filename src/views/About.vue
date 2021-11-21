ec49ee1580c3330f2f5b6ffc816033febd325dbc
tiwo <tiwocode@gmail.com>
2021-11-21 01:38:28 +0100

    add /debug route

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,7 @@
 <template>
 <div class=About>
     <h1>About this app</h1>
+    <p>This apps display your kast sms messages</p>
+    <q-btn :to="{name:'TechnicalInfo'}" no-caps flat label="technical info" />
 </div>
 </template>
--- src/views/TechnicalInfo.vue
+++ src/views/TechnicalInfo.vue
@@ -1,5 +1,47 @@
+<script setup>
+import { ref } from 'vue';
+import HelpfulHackingDisplay from '../components/HelpfulHackingDisplay.vue'
+
+const tab = ref('buildinfo');
+
+const build_info = JSON.stringify(import.meta.env, null, 2)
+
+</script>
+
 <template>
-<div class=TechnicalInfo>
-        <HelpfulHackingDisplay />
-</div>
+  <div class="q-pa-md">
+    <div class="q-gutter-y-md">
+      <q-card>
+        <q-tabs
+          v-model="tab"
+          dense
+          class="text-grey"
+          active-color="primary"
+          indicator-color="primary"
+          align="justify"
+          narrow-indicator
+        >
+          <q-tab name="state" label="state" />
+          <q-tab name="buildinfo" label="build info" />
+        </q-tabs>
+
+        <q-separator />
+
+        <q-tab-panels v-model="tab" animated >
+
+          <q-tab-panel name="state" >
+            <div class="text-h6">App state:</div>
+            <HelpfulHackingDisplay />
+          </q-tab-panel>
+
+          <q-tab-panel name="buildinfo">
+            <div class="text-h6">Build information:</div>
+            <pre>{{ build_info }}</pre>
+          </q-tab-panel>
+
+        </q-tab-panels>
+      </q-card>
+
+    </div>
+  </div>
 </template>
